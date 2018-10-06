a4d666e6dddf76002120a2b7734f484040ded94d
maari1998 <43901591+maari1998@users.noreply.github.com>
2018-10-06 14:32:17 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -18,14 +18,16 @@ class Main
 	 case 'E'  :
 	 case 'I'   :
 	 case 'O'  :
-	 case 'U'  :i++;
+	 case 'U'  :
+	case '$':i++;		
 	}
 	if(i==1)
 	System.out.println("Entered character "+ch+" is  Vowel");
 	else 
 		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
 		System.out.println("Entered character "+ch+" is Consonent");
+	
 		else
-		System.out.println("Not an alphabet");		
+			System.out.print("invalid");
 	}
 }
