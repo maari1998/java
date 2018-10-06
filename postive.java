b4e9d713ff8fb21f21cb80b01eb16f5fac7557c9
maari1998 <43901591+maari1998@users.noreply.github.com>
2018-10-06 12:30:56 +0530

    Add files via upload

---
+++ postive.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ class Main
+{
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number you want tocheck:");
+        n = s.nextInt();
+        if(n > 0)
+        {
+            System.out.println("The given number "+n+" is Positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println("The given number "+n+" is Negative");
+        }
+        else
+        {
+            System.out.println("The given number "+n+" is neither Positive nor Negative ");
+        }
+    }
+}
+
